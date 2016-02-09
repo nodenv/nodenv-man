471d7170ce8b9a25a35a63aa84f8cab84b615d87
Jason Karns <jason@karns.name>
2016-02-09 09:18:46 -0500

    simplify test setup

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,12 +1,7 @@
 load ../node_modules/bats-assert/all
 load ../node_modules/bats-mock/stub
 
-# guard against executing this block twice due to bats internals
-if [ "$NODENV_ROOT" != "${BATS_TMPDIR}/nodenv" ]; then
-  export NODENV_ROOT="${BATS_TMPDIR}/nodenv"
-
-  PATH=/usr/bin:/bin:/usr/sbin:/sbin
-  PATH="${BATS_TEST_DIRNAME}/bin:$PATH"
-  PATH="${BATS_TEST_DIRNAME}/../bin:$PATH"
-  export PATH
-fi
+PATH=/usr/bin:/bin:/usr/sbin:/sbin
+PATH="${BATS_MOCK_BINDIR}:$PATH"
+PATH="${BATS_TEST_DIRNAME}/../bin:$PATH"
+export PATH
